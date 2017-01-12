e35c923eddc4278c21070c1857c2675be0310769
Paul Vaden <pvaden@uw.edu>
2017-01-12 15:14:02 -0800

    finished 1,2,4

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,18 +1,18 @@
 # Exercise 1: Creating and Indexing Vectors
 
-# Create a vector `x` that has the values 10 through 20 in it (using the `:` operator)
+# Create a vector `x` that has the values 10 through 20 in it (using the : operator)
 
 
-# Create a vector `y` that has the values 21 through 30 in it (using the `seq` operator)
+# Create a vector `y` that has the values 21 through 30 in it (using the seq() function)
 
 
 # Create a vector `z` by combining the vectors `x` and `y`
 
 
-# Create a variable `w` that is equal to the 11th element in your vector `z`
+# Create a variable `w` that contains the 11th element in your vector `z`
 
 
-# Create a vector `q` that is equal to the second through the 5 elements of `z`
+# Create a vector `q` that contains the second through the 5 elements of `z`
 
 
 # Create a variable `len` that is equal to the length of your vector `q`
@@ -20,7 +20,8 @@
 
 ### Bonus ###
 
-# Create a vector `odd` that holds the odd numbers from 1 to 100
+# Create a vector `even` that holds the even numbers from 1 to 100
 
 
-# Using the `all` function, confirm that all of the numbers in your `odd` vector are odd
+# Using the `all()` function and `%%` (modulo) operator, confirm that all of the
+# numbers in your `even` vector are even
